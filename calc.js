f7067d0c4c9e0c032289588dfce0a1b8d66083c1
Henrike-PB <146462607+Henrike-PB@users.noreply.github.com>
2024-04-05 15:27:32 -0300

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,7 +1,7 @@
 function calcular() {
     const valor = document.getElementById('valor').value;
     const calculo = valor * 5.04;
-    document.getElementById('dolar').textContent = 'dolar é:' + calculo.toFixed(2);
+    document.getElementById('dolar').innerHTML = 'dolar é:' + calculo.toFixed(2);
 }
 
-calcular();
+
